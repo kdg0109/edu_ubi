906ba8f69b9e6e63c72cc95f3423bb3b44d8e204
donggeonkim <donggeon.kim@ubivelox.com>
2018-05-25 16:08:23 +0900

    lookup-table 활용

--- edu_project/src/com/ubivelox/changehexa/HexAndByteArray.java
+++ edu_project/src/com/ubivelox/changehexa/HexAndByteArray.java
@@ -1,100 +1,122 @@
 package com.ubivelox.changehexa;
 
-import java.awt.image.LookupOp;
-
 import com.ubivelox.changehexa.exception.UbiveloxException;
 import com.ubivelox.contents.LookUpTable;
- 
-public class HexAndByteArray {
 
-	static LookUpTable lookUp = new LookUpTable();
-	
-	
-	public static String ByteToHex(final byte[] byteArr) throws UbiveloxException
-	{  
+public class HexAndByteArray
+{
+
+    static LookUpTable lookUp = new LookUpTable();
+
+
+
+
+
+    public static String ByteToHex(final byte[] byteArr) throws UbiveloxException
+    {
 
         StringBuffer hexStr = new StringBuffer();
 
         for ( byte byteVal : byteArr )
         {
-            hexStr.append(getHex(byteVal)); 
-        }  
-
-        return hexStr.toString();
-    }
-	
-	public static String ByteToHexUseLookUpTable1(final byte[] byteArr) throws UbiveloxException
-	{ 
-        StringBuffer hexStr = new StringBuffer();
- 
-        for ( byte byteVal : byteArr )
-        { 
-        	hexStr.append(lookUp.mapping(byteVal));
-        }
-
-        return hexStr.toString();
-    }
-	
-	public static String ByteToHexUseLookUpTable2(final byte[] byteArr) throws UbiveloxException
-	{ 
-        StringBuffer hexStr = new StringBuffer();
- 
-        for ( byte byteVal : byteArr )
-        { 
-        	hexStr.append(lookUp.hashmapping(byteVal));
+            hexStr.append(getHex(byteVal));
         }
 
         return hexStr.toString();
     }
 
-	private static String getHex(final int byteVal) {
-		int byteValTmp = 0;
-		StringBuffer hexStr = new StringBuffer();
-		
-		String hexaStrFirst = "";
-		String hexaStrSecond = "";
-		String hexaTmp = "";
 
-		byteValTmp = byteVal;
-		
-		 
-		do {
-			hexaTmp = "";
-			if (byteValTmp % 16 > 9) {
-				switch (byteValTmp % 16) {
-				case 10:
-					hexaTmp = "A";
-					break;
-				case 11:
-					hexaTmp = "B";
-					break;
-				case 12:
-					hexaTmp = "C";
-					break;
-				case 13:
-					hexaTmp = "D";
-					break;
-				case 14:
-					hexaTmp = "E";
-					break;
-				case 15:
-					hexaTmp = "F";
-				}
-			} else { 
-				hexaTmp += byteValTmp % 16;
-			}
-			if(hexaStrSecond.length() <= 0) {
-				hexaStrSecond = hexaTmp;
-			}else {
-				hexaStrFirst = hexaTmp;
-			}
-			
-			byteValTmp /= 16;
-		} while (byteValTmp > 0);
-		
-		
 
-//		System.out.println(hexaStrFirst+hexaStrSecond);
-		return hexStr.append(hexaStrFirst).append(hexaStrSecond).toString();
-	}
+
+
+    public static String ByteToHexUseLookUpTable1(final byte[] byteArr) throws UbiveloxException
+    {
+        StringBuffer hexStr = new StringBuffer();
+
+        for ( byte byteVal : byteArr )
+        {
+            hexStr.append(lookUp.mapping(byteVal));
+        }
+
+        return hexStr.toString();
+    }
+
+
+
+
+
+    public static String ByteToHexUseLookUpTable2(final byte[] byteArr) throws UbiveloxException
+    {
+        StringBuffer hexStr = new StringBuffer();
+
+        for ( byte byteVal : byteArr )
+        {
+            hexStr.append(lookUp.hashmapping(byteVal));
+        }
+
+        return hexStr.toString();
+    }
+
+
+
+
+
+    private static String getHex(final int byteVal)
+    {
+        int byteValTmp = 0;
+        StringBuffer hexStr = new StringBuffer();
+
+        String hexaStrFirst = "";
+        String hexaStrSecond = "";
+        String hexaTmp = "";
+
+        byteValTmp = byteVal;
+
+        do
+        {
+            hexaTmp = "";
+            if ( byteValTmp % 16 > 9 )
+            {
+                switch ( byteValTmp % 16 )
+                {
+                    case 10:
+                        hexaTmp = "A";
+                        break;
+                    case 11:
+                        hexaTmp = "B";
+                        break;
+                    case 12:
+                        hexaTmp = "C";
+                        break;
+                    case 13:
+                        hexaTmp = "D";
+                        break;
+                    case 14:
+                        hexaTmp = "E";
+                        break;
+                    case 15:
+                        hexaTmp = "F";
+                }
+            }
+            else
+            {
+                hexaTmp += byteValTmp % 16;
+            }
+            if ( hexaStrSecond.length() <= 0 )
+            {
+                hexaStrSecond = hexaTmp;
+            }
+            else
+            {
+                hexaStrFirst = hexaTmp;
+            }
+
+            byteValTmp /= 16;
+        }
+        while ( byteValTmp > 0 );
+
+        return hexStr.append(hexaStrFirst)
+                     .append(hexaStrSecond)
+                     .toString();
+    }
 }
--- edu_project/src/com/ubivelox/changehexa/test/Test.java
+++ edu_project/src/com/ubivelox/changehexa/test/Test.java
@@ -7,13 +7,18 @@ import org.junit.BeforeClass;
 import com.ubivelox.changehexa.HexAndByteArray;
 import com.ubivelox.changehexa.exception.UbiveloxException;
 
-public class Test {
+public class Test
+{
 
     // StaticValue 상수 사용 부분
     static String hexaString = "";
     static byte[] byteString = null;
-    
+
     int           loop       = 500000;
+
+
+
+
 
     // 관련 객체 정보 초기화
     @BeforeClass
@@ -24,43 +29,58 @@ public class Test {
         hexaString = StaticValue.HEXA_STRING_ARRAY.get(0);
         byteString = StaticValue.BYTE_STRING_ARRAY.get(0);
     }
-    
-	@org.junit.Test
-	public void testByteToHex() throws UbiveloxException {
-		long start = System.currentTimeMillis();
+
+
+
+
+
+    @org.junit.Test
+    public void testByteToHex() throws UbiveloxException
+    {
+        long start = System.currentTimeMillis();
         long end;
         for ( int i = 0; i < this.loop; i++ )
         {
-            assertEquals(StaticValue.HEXA_STRING_ARRAY.get(i%17), HexAndByteArray.ByteToHex(StaticValue.BYTE_STRING_ARRAY.get(i%17)));
+            assertEquals(StaticValue.HEXA_STRING_ARRAY.get(i % 17), HexAndByteArray.ByteToHex(StaticValue.BYTE_STRING_ARRAY.get(i % 17)));
         }
         end = System.currentTimeMillis();
         double resultTime = (end - start) / 1000.0;
-        System.out.println("lookup table 미사용 변환 시간 : " + resultTime + "   ");
-	}
+        System.out.println("lookup table 미사용 변환 시간 : " + resultTime);
+    }
 
-	@org.junit.Test
-	public void testByteToHexUseLookUpTable1() throws UbiveloxException {
-		long start = System.currentTimeMillis();
-        long end; 
+
+
+
+
+    @org.junit.Test
+    public void testByteToHexUseLookUpTable1() throws UbiveloxException
+    {
+        long start = System.currentTimeMillis();
+        long end;
         for ( int i = 0; i < this.loop; i++ )
         {
-            assertEquals(StaticValue.HEXA_STRING_ARRAY.get(i%17), HexAndByteArray.ByteToHexUseLookUpTable1(StaticValue.BYTE_STRING_ARRAY.get(i%17)));
-        }  
-        end = System.currentTimeMillis();
-        double resultTime = (end - start) / 1000.0;
-        System.out.println("lookup table(String[]) 사용 변환 시간 : " + resultTime + "   ");
-	}
-
-	@org.junit.Test
-	public void testByteToHexUseLookUpTable2() throws UbiveloxException {
-		long start = System.currentTimeMillis();
-        long end; 
-        for ( int i = 0; i < this.loop; i++ )
-        {
-            assertEquals(StaticValue.HEXA_STRING_ARRAY.get(i%17), HexAndByteArray.ByteToHexUseLookUpTable2(StaticValue.BYTE_STRING_ARRAY.get(i%17)));
+            assertEquals(StaticValue.HEXA_STRING_ARRAY.get(i % 17), HexAndByteArray.ByteToHexUseLookUpTable1(StaticValue.BYTE_STRING_ARRAY.get(i % 17)));
         }
         end = System.currentTimeMillis();
         double resultTime = (end - start) / 1000.0;
-        System.out.println("lookup table(HashMap) 사용 변환 시간 : " + resultTime + "   ");
-	}
+        System.out.println("lookup table(String[]) 사용 변환 시간 : " + resultTime);
+    }
+
+
+
+
+
+    @org.junit.Test
+    public void testByteToHexUseLookUpTable2() throws UbiveloxException
+    {
+        long start = System.currentTimeMillis();
+        long end;
+        for ( int i = 0; i < this.loop; i++ )
+        {
+            assertEquals(StaticValue.HEXA_STRING_ARRAY.get(i % 17), HexAndByteArray.ByteToHexUseLookUpTable2(StaticValue.BYTE_STRING_ARRAY.get(i % 17)));
+        }
+        end = System.currentTimeMillis();
+        double resultTime = (end - start) / 1000.0;
+        System.out.println("lookup table(HashMap) 사용 변환 시간 : " + resultTime);
+    }
 }
